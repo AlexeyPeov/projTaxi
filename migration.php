7c73faf9a74e34ef3d09bca2b7b0b7e71f0b0214
AlexeyPeov <111562626+AlexeyPeov@users.noreply.github.com>
2023-03-03 19:09:47 +0300

    Moved the project to laravel framework and pgsql db. Finished migrating order logic.

--- migration.php
+++ migration.php
@@ -11,37 +11,37 @@ class Migration
 
     public function up()
     {
-        $create = "
-            CREATE TABLE car (
-            id INT NOT NULL AUTO_INCREMENT,
-            brand VARCHAR(50) NOT NULL,
-            plates VARCHAR(20) NOT NULL,
-            color VARCHAR(50) NOT NULL,
+        $create = '
+            CREATE TABLE "car" (
+            id SERIAL,
+            brand CHAR(50) NOT NULL,
+            plates CHAR(20) NOT NULL,
+            color CHAR(50) NOT NULL,
             carClass INT NOT NULL,
             PRIMARY KEY (id)
                                
         );
 
-        CREATE TABLE customer (
-            id INT NOT NULL AUTO_INCREMENT,
-            firstName VARCHAR(50),
-            secondName VARCHAR(50),
-            birthday DATETIME,
+        CREATE TABLE "customer" (
+            id SERIAL,
+            firstName CHAR(50),
+            secondName CHAR(50),
+            birthday TIMESTAMP,
             personalDiscount INT,
-            phoneNum VARCHAR(20) NOT NULL,
+            phoneNum CHAR(20) NOT NULL,
             orderCount INT,
             orderDeclinedCount INT,
             PRIMARY KEY (id)
         );
 
-        CREATE TABLE taxidriver (
-            id INT NOT NULL AUTO_INCREMENT,
-            firstName VARCHAR(50) NOT NULL ,
-            secondName VARCHAR(50) NOT NULL ,
-            birthday DATETIME NOT NULL ,
+        CREATE TABLE "taxidriver" (
+            id SERIAL,
+            firstName CHAR(50) NOT NULL,
+            secondName CHAR(50) NOT NULL,
+            birthday TIMESTAMP NOT NULL,
             experience INT,
             rating FLOAT,
-            qualification varchar(20) NOT NULL,
+            qualification char(20) NOT NULL,
             carDriving INT,
             reviewHeap INT,
             reviewsGiven INT,
@@ -49,34 +49,42 @@ class Migration
             FOREIGN KEY(carDriving) REFERENCES car(id)
         );
 
-        CREATE TABLE `order` (
-            id INT NOT NULL AUTO_INCREMENT,
+        CREATE TABLE "order" (
+            id SERIAL,
             orderStatus INT NOT NULL,
             customerId INT,
             taxiDriverId INT,
             class INT NOT NULL,
             price FLOAT NOT NULL,
-            pointA VARCHAR(50) NOT NULL,
-            pointB VARCHAR(50) NOT NULL,
-            dayCreated DATETIME NOT NULL,
+            pointA CHAR(50) NOT NULL,
+            pointB CHAR(50) NOT NULL,
+            dayCreated TIMESTAMP NOT NULL,
             reviewGiven BOOLEAN,
             PRIMARY KEY (id),
             FOREIGN KEY(taxiDriverId) REFERENCES taxidriver(id),
-           FOREIGN KEY (customerId) REFERENCES customer(id)
+            FOREIGN KEY (customerId) REFERENCES customer(id)
         );
-        ";
-        $this->connection->query($create);
+        ';
     }
 
     public function down()
     {
-        $delete = "
-                DROP TABLE `order`;
+        $delete = '
+                DROP TABLE "order";
                 DROP TABLE taxidriver;
                 DROP TABLE car;
                 DROP TABLE customer;
             
-        ";
+        ';
+        $this->connection->query($delete);
+    }
+
+    public function downCar()
+    {
+        $delete = '
+            DROP TABLE "car";
+        ';
+
         $this->connection->query($delete);
     }
 }
---
+++ taxi/resources/views/orders/submit.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Phone Number</title>
+</head>
+<body>
+<p>submitted</p>
+{{--<form action="/submit" method="POST">
+    @csrf
+    <label for="phone">Phone Number:</label><br>
+    <input type="text" id="phone" name="phone"><br><br>
+
+    <input type="submit" value="Submit">
+</form>--}}
+</body>
+</html>
