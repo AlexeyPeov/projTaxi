caa431e7917ddd29bca0acd2aa2561dbf3d4e955
AlexeyPeov <111562626+AlexeyPeov@users.noreply.github.com>
2023-03-13 13:53:47 +0300

    added /vendor to .gitignore

--- src/Views/whoami.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$content = '
-    <div class="center">
-        <button class="btn" id="passenger" onclick="location.href=\'/customer\'">I\'m a passenger</button>
-        <button class="btn" id="driver" onclick="location.href=\'/taxidriver\'">I\'m a driver</button>
-    </div>
-';
-include __DIR__ . "/app.php";
